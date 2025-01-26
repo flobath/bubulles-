c2d8c17294ec885e5e3d550433fd6b080c4e4090
flobath <114670433+flobath@users.noreply.github.com>
2025-01-26 14:08:10 +0100

    [FIX] dontdestroyonload

--- Assets/CafeScene/DontDestroyOnLoadScript.cs
+++ Assets/CafeScene/DontDestroyOnLoadScript.cs
@@ -1,9 +1,34 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 public class DontDestroyOnLoadScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake() {
-        DontDestroyOnLoad(this.gameObject);
+    private static Dictionary<string, GameObject> _instances = new Dictionary<string, GameObject>();
+    [SerializeField] private string ID; // HACK: This ID can be pretty much anything, as long as you can set it from the inspector
+
+    void Awake()
+    {
+        if(_instances.ContainsKey(ID))
+        {
+            var existing = _instances[ID];
+
+            // A null result indicates the other object was destoryed for some reason
+            if(existing != null)
+            {
+                if(ReferenceEquals(gameObject, existing))
+                    return;
+
+                Destroy(gameObject);
+
+                // Return to skip the following registration code
+                return;
+            }
+        }
+
+        // The following code registers this GameObject regardless of whether it's new or replacing
+        _instances[ID] = gameObject;
+
+        DontDestroyOnLoad(gameObject);
     }
 }
